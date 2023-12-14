7901c10555c02c57a9321e8dbd33816533573178
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-12-13 22:37:56 -0300

    feat: implements configuration page

--- lib/pages/configuration_page.dart
+++ lib/pages/configuration_page.dart
@@ -8,8 +8,42 @@ class ConfigurationPage extends StatefulWidget {
 }
 
 class _ConfigurationPageState extends State<ConfigurationPage> {
+  TextEditingController nameController = TextEditingController(text: "");
+  TextEditingController heightController = TextEditingController(text: "");
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: const Text('to do'));
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+      child: ListView(
+        children: [
+          const Center(
+            child: Text(
+              'Configurações',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Nome",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextField(
+            controller: nameController,
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            "Altura",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          TextField(
+            keyboardType: TextInputType.number,
+            controller: heightController,
+          ),
+          const SizedBox(height: 24),
+          FilledButton(onPressed: () {}, child: const Text('Salvar'))
+        ],
+      ),
+    );
   }
 }
