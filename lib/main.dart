2159e35a84644a627cdf6d3f81a7e5b18d5bcc98
Rodolfo HiOk <rodolfo.okino@gmail.com>
2023-12-13 22:14:50 -0300

    feat: config hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 import 'package:imc_flutter/imc_app.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  var documentDirectory =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(documentDirectory.path);
   runApp(const IMCApp());
 }
